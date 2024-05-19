74c9045eefec88cf0c990e409e38b1aa7ee3f337
chaewonkim <kch9870@naver.com>
2024-05-19 17:21:25 +0900

    Compose에서의 상태
    -. recomposition 방지를 위한 remember 사용
    -. mutableStateOf 사용

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelab.ui.theme.BasicCodeLabTheme
@@ -43,19 +45,25 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text(text = "Show More")
+                Text(if (expanded.value) "Show less" else "Show More")
             }
         }
     }
