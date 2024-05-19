06fd965ba34d8824a5123afbb6e3e3ab6108af28
chaewonkim <kch9870@naver.com>
2024-05-19 17:51:40 +0900

    Lazy List
    -. RecycleView 와 같이 LazyColumn LazyRow 사용
    -. item을 사용하여 값을 지정할 수 있음

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -70,9 +72,12 @@ fun OnboardingScreen(
 }
 
 @Composable
-fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(20) { "List: $it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
